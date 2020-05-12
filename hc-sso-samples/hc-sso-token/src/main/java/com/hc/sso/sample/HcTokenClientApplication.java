c152a027395b7e311c1805bbbe17fe9e4fc213f9
donghuajie <donghuajie@xmi01.com>
2020-05-12 11:24:15 +0800

    init：sso-token-sample

---
+++ hc-sso-samples/hc-sso-token/src/main/java/com/hc/sso/sample/HcTokenClientApplication.java
@@ -0,0 +1,16 @@
+package com.hc.sso.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author: HuaChenG
+ * @Description:
+ * @Date: Create in 2020/05/12
+ */
+@SpringBootApplication
+public class HcTokenClientApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(HcTokenClientApplication.class, args);
+    }
+}
